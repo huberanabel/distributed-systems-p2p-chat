6d1b5aa86b140259ca0755040795343da80bc9a4
huberanabel <132076404+huberanabel@users.noreply.github.com>
2026-06-26 11:31:13 +0200

    Code

---
+++ p2p_chat/main.py
@@ -0,0 +1,42 @@
+import threading
+
+from peer import Peer
+
+
+def main():
+    username = input("Username: ")
+    port = int(input("Your port: "))
+
+    peer = Peer(
+        username=username,
+        host="127.0.0.1",
+        port=port
+    )
+
+    listener_thread = threading.Thread(
+        target=peer.start_listener,
+        daemon=True
+    )
+    listener_thread.start()
+
+    connect = input("Connect to another peer? y/n: ")
+
+    if connect.lower() == "y":
+        peer_host = input("Peer host: ")
+        peer_port = int(input("Peer port: "))
+        peer.connect_to_peer(peer_host, peer_port)
+
+    print("\nYou can now write messages.")
+    print("Type 'exit' to stop.\n")
+
+    while True:
+        text = input()
+
+        if text.lower() == "exit":
+            break
+
+        peer.send_message(text)
+
+
+if __name__ == "__main__":
+    main()
